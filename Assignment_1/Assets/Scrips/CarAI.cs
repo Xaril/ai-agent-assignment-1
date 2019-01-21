d7b4a113224b33148a0f06701fe8e0ea9657be34
Fredrik Omstedt <fredrikomstedt@n144-p143.eduroam.kth.se>
2019-01-21 14:06:06 +0100

    Added unity project

---
+++ Assignment_1/Assets/Scrips/CarAI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof(CarController))]
+    public class CarAI : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+
+        public GameObject terrain_manager_game_object;
+        TerrainManager terrain_manager;
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+            terrain_manager = terrain_manager_game_object.GetComponent<TerrainManager>();
+
+            // Plan your path here
+            // ...
+        }
+
+
+        private void FixedUpdate()
+        {
+            // Execute your path here
+            // ...
+
+            // this is how you access information about the terrain
+            int i = terrain_manager.myInfo.get_i_index(transform.position.x);
+            int j = terrain_manager.myInfo.get_j_index(transform.position.z);
+            float grid_center_x = terrain_manager.myInfo.get_x_pos(i);
+            float grid_center_z = terrain_manager.myInfo.get_z_pos(j);
+
+            Debug.DrawLine(transform.position, new Vector3(grid_center_x, 0f, grid_center_z));
+
+
+            // this is how you control the car
+            m_Car.Move(1f, 1f, 1f, 0f);
+
+        }
+    }
+}
